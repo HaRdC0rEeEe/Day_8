0719464fe3baa85f28dca425f56b75c957954088
HaRdC0rEeEe <petrpospi999@seznam.cz>
2023-07-20 12:30:50 +0200

    introduced closeable modals in modal-starter

---
+++ modal_starter/script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+
+function initEventListeners() {
+    overlay.addEventListener('click', function () {
+        hide();
+    })
+
+    closeModal.addEventListener('click', function () {
+        hide();
+    })
+
+    document.addEventListener('keydown', evt => {
+        if (evt.key === 'Escape')
+            hide();
+    })
+
+
+}
+
+function hide() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+initEventListeners();
+
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', function () {
+        modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+    });
+}
+
+
+
+
+
